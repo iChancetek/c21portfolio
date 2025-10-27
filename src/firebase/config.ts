06e53a5fb1cd57f9b64b0bcf0df3c98cf517e2d2
ChanceTEK, LLC - MSP <chanceminus@gmail.com>
2025-10-27 19:34:30 +0000

    Add all firebase variable for project to .env

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-1594258539-a60ba",
-  "appId": "1:174490017685:web:90998e6abec8ced3c1a09c",
-  "apiKey": "AIzaSyDYQds8q66ePdupmR0EwESe9mBEgjfMsUw",
-  "authDomain": "studio-1594258539-a60ba.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "174490017685"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
