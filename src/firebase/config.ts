e57ead3e76bba4fc9eb0d3d304c7feff5e141ec5
ChanceTEK, LLC - MSP <chanceminus@gmail.com>
2025-10-27 18:16:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1594258539-a60ba",
+  "appId": "1:174490017685:web:90998e6abec8ced3c1a09c",
+  "apiKey": "AIzaSyDYQds8q66ePdupmR0EwESe9mBEgjfMsUw",
+  "authDomain": "studio-1594258539-a60ba.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "174490017685"
+};
