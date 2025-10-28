308529982740f466a8a93c162605eb785586df5d
ChanceTEK, LLC - MSP <chanceminus@gmail.com>
2025-10-28 03:25:07 +0000

    No improvement. Looks the same.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body className={cn('font-body antialiased min-h-screen bg-background flex flex-col')} suppressHydrationWarning>
+      <body className={cn('min-h-screen bg-background font-sans antialiased flex flex-col')} suppressHydrationWarning>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
