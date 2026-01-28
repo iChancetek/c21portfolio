738c4264901299f90949a4e34c9f5164229e6ed4
ChanceTEK, LLC - MSP <chanceminus@gmail.com>
2026-01-28 18:14:52 +0000

    Will the PWA work without an icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#01091f" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="https://images.unsplash.com/photo-1523961131990-5ea7c61b2107?w=192&h=192&fit=crop" />
       </head>
       <body className={cn('min-h-screen bg-background font-sans antialiased flex flex-col')} suppressHydrationWarning>
         <Providers>
